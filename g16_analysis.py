266b00a4722f594edf52c5a7a9dec5af8f143a3a
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2018-10-16 21:46:15 -0400

    Added G3 parsing to analysis

--- g16_analysis.py
+++ g16_analysis.py
@@ -4,12 +4,16 @@ import pandas as pd
 from glob import glob
 
 def parse_log(filepath):
+    """ Function for parsing a G16 logfile.
+    """
     frequencies = list()
     filename = filepath.split("/")[-1].split(".log")[0].split("-")
     isomer = filename[0]
     multi = filename[1]
     ZPE = 0.
     total = 0.
+    G3 = 0.
+    success = False
     with open(filepath) as read_file:
         for line in read_file:
             if "Zero-point correction=" in line:
@@ -19,10 +23,16 @@ def parse_log(filepath):
             if "Frequencies --" in line:
                 current = line.split()[2:]
                 frequencies.extend([float(freq) for freq in current])
+            if "Normal termination" in line:
+                success = True
+            if "G3(0 K)" in line:
+                G3 = float(line.split()[2])
     data = {
         "Isomer": isomer,
         "Multiplicity": multi,
+        "success": success,
         "ZPE": ZPE,
+        "G3-0K": G3,
         "E+ZPE": total,
         "Frequencies": frequencies
         }
@@ -37,6 +47,7 @@ def main():
     df["E"] = df["E+ZPE"] - df["ZPE"]
     df["Relative-0K"] = conversion * (df["E+ZPE"] - df["E+ZPE"].min())
     df["Relative-E"] = conversion * (df["E"] - df["E"].min())
+    df["Relative-G3"] = conversion * (df["G3-0K"] - df["G3-0K"].min())
     df.to_csv("collected_results.csv")
 
 
