0d736885c7ca3cf32652d6cb54cf3c360492eb8b
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2019-07-02 21:39:11 -0400

    Adding a pubchem parser to XYZ

---
+++ gen_pubchem.py
@@ -0,0 +1,42 @@
+
+import os
+
+import pubchempy
+import click
+
+import utils
+
+
+def query_pubchem(formula, max_count=100):
+    df = pubchempy.get_compounds(
+            formula,
+            "formula",
+            listkey_count=max_count
+            as_dataframe=True
+            )
+    return df
+
+
+def df2xyz(pubchem_df):
+    # Write smiles to file
+    smi = pubchem_df.'canonical_smiles'.values
+    with open("molecules.smi", "w+") as write_file:
+        for value in smi:
+            write_file.write(value + "\n")
+    os.mkdir("structures")
+    utils.smi2xyz("molecules.smi")
+
+
+
+@click.command()
+@click.argument("formula")
+@click.option("max_count", default=100, help="Maximum number of queries to return.")
+def main(formula, max_count=100):
+    df = query_pubchem(formula, max_count)
+    df.to_csv("pubchem_query.csv")
+    df2xyz(df)
+
+
+if __name__ == "__main__":
+    main()
+
