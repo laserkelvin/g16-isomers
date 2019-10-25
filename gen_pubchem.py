05c974b5bdd2dbf278a6d79a18b9a7c9126682b7
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2019-10-25 09:36:32 -0400

    Now escapes bad formula requests

--- gen_pubchem.py
+++ gen_pubchem.py
@@ -11,13 +11,16 @@ import utils
 
 
 def query_pubchem(formula, max_count=100):
-    df = pubchempy.get_compounds(
-            formula,
-            "formula",
-            listkey_count=max_count,
-            as_dataframe=True
-            )
-    return df
+    try:
+        df = pubchempy.get_compounds(
+                formula,
+                "formula",
+                listkey_count=max_count,
+                as_dataframe=True
+                )
+        return df
+    except:
+        print(f"{formula} failed to query!")
 
 
 def exhaust_query(formula, max_count=100):
@@ -27,29 +30,43 @@ def exhaust_query(formula, max_count=100):
     """
     batch = list()
     pubchem_df = query_pubchem(formula, max_count)
-    batch.append(pubchem_df)
-    start = len(pubchem_df)
-    while len(pubchem_df) == max_count:
-        if start >= 500:
-            break
-        pubchem_df = query_pubchem(formula, max_count)
-        start += len(pubchem_df)
+    if pubchem_df is not None:
         batch.append(pubchem_df)
-    # Combine the dataframes together
-    full_df = pd.concat(batch)
-    return full_df
+        start = len(pubchem_df)
+        while len(pubchem_df) == max_count:
+            if start >= 500:
+                break
+            pubchem_df = query_pubchem(formula, max_count)
+            start += len(pubchem_df)
+            batch.append(pubchem_df)
+        # Combine the dataframes together
+        full_df = pd.concat(batch)
+        return full_df
     
 
 def df2xyz(pubchem_df):
+    """
+    Function to generate XYZ files from a Pandas
+    DataFrame from pubchem.
+
+    Because PubChem also returns isotopologues,
+    we will also prefilter the dataframe to remove
+    them, and use the isomeric smiles instead of
+    canonical smiles.
+    """
     # Write smiles to file
-    smi = pubchem_df["canonical_smiles"].values
+    subset = pubchem_df.loc[
+        ~pubchem_df["iupac_name"].str.contains("deut")
+        ]
+    # Get only unique smiles entries, so we don't do
+    # redundant calculations
+    smi = subset["isomeric_smiles"].unique()
     with open("molecules.smi", "w+") as write_file:
         for value in smi:
             write_file.write(value + "\n")
     if os.path.isdir("structures") is False:
         os.mkdir("structures")
     utils.smi2xyz("molecules.smi")
-
 
 
 @click.command()
